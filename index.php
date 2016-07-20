2aa6878d78de1ee297d8628d38015629e764c736
Sissel <907886076@qq.com>
2016-07-20 21:14:59 +0800

    我就试试git 顺便改一下文件结构

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>XX公司XX系统</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+	
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#公司网站" >XX公司</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li  class="active"><a href="index.php">网站首页</a></li>
+              <li><a href="view.php">信息查询</a></li>
+              <li><a href="input.php">信息录入</a></li>
+              <li><a href="logout.php">退出登录</a></li>
+              <li><a href="change.php">修改密码</a></li>
+            </ul>
+            <form action="##" class="navbar-form pull-right" rol="search">
+              <div class="form-group">
+				<input type="text" class="form-control" placeholder="请输入关键词" />
+				<button type="submit" class="btn btn-default">搜索</button>
+			  </div>
+              
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+	  
+	  
+	  
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+        <h1>公司简介</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a href="#" class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+      </div>
+
+      <hr>
+
+      <footer>
+        <p>&copy; Company 2016</p>
+      </footer>
+
+    </div> <!-- /container -->
+
+	
+    <script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
+	
+  </body>
+</html>
---
+++ libs/footer.php
@@ -0,0 +1,12 @@
+<hr>
+<center>
+<footer class="blog-footer">
+	<p><b>公司名称</b> All rights reserved. </p>
+	<p>
+		<a href="#">联系我们</a>
+		<a href="#">意见反馈</a>
+		<a href="#">加入我们</a>
+	</p>
+</footer>
+</center>
+
